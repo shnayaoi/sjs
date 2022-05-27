9ff42403ba90558b10e2ba92c70daf36c79c0726
shnayuoi <shnamilk@gmail.com>
2022-05-27 09:02:53 +0300

    вывод текста на третей секунде

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 console.log('frog');
-document.getElementById('current').innerText='desck';
+const video = document.querySelector('video');
+const text = document.getElementById('current');
+text.innerText = 'desck';
+video.addEventListener('timeupdate', (event) => {
+    console.log(video.currentTime);
+    if(video.currentTime>3) {
+        text.innerText = 'Bom';
+    }
+});
